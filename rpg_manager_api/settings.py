1bcf29bc27e41d07f644d5b5f5884b3dcff98cfb
Pedro Gomes <pedrodetoledoh@gmail.com>
2024-09-01 14:25:19 -0600

    Auth test

--- rpg_manager_api/settings.py
+++ rpg_manager_api/settings.py
@@ -73,7 +73,9 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://13.59.119.206/",
+]
 
 ROOT_URLCONF = 'rpg_manager_api.urls'
 
