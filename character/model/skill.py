6e451e9e307ac0d6a0715d99eccd413f2a12bc4a
Pedro Gomes <pedrodetoledoh@gmail.com>
2024-08-31 20:47:30 -0600

    Auth and skill fix

--- character/model/skill.py
+++ character/model/skill.py
@@ -6,7 +6,7 @@ class Skill(models.Model):
     name = models.CharField(max_length=100)
     rank = models.CharField(max_length=50)
     type = models.CharField(max_length=50)
-    element = models.CharField(max_length=50)
+    school = models.CharField(max_length=50)
     description = models.TextField()
 
     def __str__(self):
